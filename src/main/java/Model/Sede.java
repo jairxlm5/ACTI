a0e7d7161edde1b4a56e2337071192e210d5c069
Daniel Penado <penadodan02@gmail.com>
2022-03-10 15:24:31 -0600

    Clase sede agregada

---
+++ src/main/java/Model/Sede.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author danielp
+ */
+public class Sede {
+    private String codigo;
+    private String nombre;
+    private String ubicacion;
+
+    public Sede(String codigo, String nombre, String ubicacion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+
+    public Sede() {
+        
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    
+    
+}
